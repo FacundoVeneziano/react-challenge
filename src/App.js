4b80274012350f83f78e5fcfef41249201c7b915
FacundoVeneziano <facundo.e.veneziano@gmail.com>
2022-09-03 23:50:01 -0300

    add next step functionality in form

--- src/App.js
+++ src/App.js
@@ -1,85 +1,56 @@
-import {
-  useState,
-  cloneElement
-} from 'react'
+import { useState, cloneElement } from "react";
 
-import {
-  Container,
-  Step,
-  Stepper,
-  StepLabel,
-  Box,
-  Grid
-} from '@mui/material'
+import { Container, Step, Stepper, StepLabel, Box, Grid } from "@mui/material";
 
-
-import Form1 from './components/Form1'
-import Form2 from './components/Form2'
-import TableData from './components/Table'
+import Form1 from "./components/Form1";
+import Form2 from "./components/Form2";
+import TableData from "./components/Table";
 
 const steps = [
   {
-    label: 'Paso 1',
-    componente: <Form1 />
+    label: "Paso 1",
+    componente: <Form1 />,
   },
   {
-    label: 'Paso 2',
-    componente: <Form2 />
-  }
-]
+    label: "Paso 2",
+    componente: <Form2 />,
+  },
+];
 
 const App = () => {
-  const [step, setStep] = useState(0)
+  const [step, setStep] = useState(0);
+
 
   return (
     <Container
       fixed
       style={{
-        padding: '3em'
+        padding: "3em",
       }}
     >
-      <Grid
-        container
-        spacing={6}
-      >
-        <Grid
-          item
-          md={4}
-          sm={12}
-          xs={12}
-        >
-          <Stepper
-            activeStep={step}
-          >
+      <Grid container spacing={6}>
+        <Grid item md={4} sm={12} xs={12}>
+          <Stepper activeStep={step}>
             {steps.map((step, key) => (
-              <Step
-                key={key}
-              >
-                <StepLabel>
-                  {step.label}
-                </StepLabel>
+              <Step key={key}>
+                <StepLabel>{step.label}</StepLabel>
               </Step>
             ))}
           </Stepper>
           <Box
             style={{
-              marginTop: '4em'
+              marginTop: "4em",
             }}
           >
-            {steps[step].componente}
+            {cloneElement(steps[step].componente, { setStep })}
           </Box>
         </Grid>
-        <Grid
-          item
-          md={8}
-          sm={12}
-          xs={12}
-        >
+        <Grid item md={8} sm={12} xs={12}>
           <TableData />
         </Grid>
       </Grid>
     </Container>
-  )
-}
+  );
+};
 
-export default App
+export default App;
